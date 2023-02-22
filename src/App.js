25335f020423440729b8a0a7d1f556468860c438
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-22 12:59:39 +0100

    startP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import Libs
+// * 1
+import { useState } from 'react';
+
+
+// import Css
 import './App.css';
 
+
+
 function App() {
+// hook bauen
+// * 2 
+const [count, setCount] = useState(0);
+
+// function bauen
+// * 4
+function increment() {
+  setCount(count + 1);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  {/*  // html bauen   
+   // * 3 */}
+   <p> {count}</p>
+
+ {/*   // * 5 onClick hinzufügen */}
+   <button onClick={increment}> + 1</button>
+
+   {/* // * 6 Szenario 2 alles 1-5 in arrow function */}
+   <button onClick={ ( ) => {setCount(count-1)}} > - 1</button>
+
+
+
     </div>
   );
 }
